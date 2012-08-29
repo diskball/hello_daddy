d4305a8705bfe60b1f8b67af60d5180ef7c72e63
George Bafaloukas <george.bafaloukas@iteam.gr>
2012-08-29 11:25:20 +0300

    Initial Commit

---
+++ Hello Daddy/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Hello Daddy
+//
+//  Created by George Bafaloukas on 8/29/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
