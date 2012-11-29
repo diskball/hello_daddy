3c0ee991b337cfa194e3fbf3b84651ed01a26433
George Bafaloukas <george.bafaloukas@iteam.gr>
2012-11-29 14:18:58 +0200

    Consts File

---
+++ Hello Daddy/Consts.h
@@ -0,0 +1,108 @@
+//
+//  Consts.h
+//  Hello Daddy
+//
+//  Created by George Bafaloukas on 9/17/12.
+//
+//
+
+//define Kills for successful levels
+#define Level1Kills        51
+#define Level2Kills        101
+#define Level3Kills        121
+#define Level4Kills        81
+#define LevelBonusKills    100
+
+//define BG Music for Levels
+#define Level1Music        @"peter_gunn.mp3"
+#define Level2Music        @"flashsperm.mp3"
+#define Level3Music        @"1984.mp3"
+#define Level4Music        @"boss.mp3"
+#define Level1MusicSec     @"bottom_gun.mp3"
+#define Level2MusicSec     @"cop_porn.mp3"
+#define Level3MusicSec     @"the_sperm_rider.mp3"
+#define Level4MusicSec     @"worm_it_out.mp3"
+#define LevelBonusMusic    @"flashsperm.mp3"
+#define MenuMusic          @"flashsperm.mp3"
+
+//define effects
+#define KillEffect         @"blast.mp3"
+#define HeartEffect        @"liveUpSound.mp3"
+#define LifeLostEffect     @"peter_gunn.mp3"
+#define CondomFired        @"scifi003.mp3"
+#define LoadingEffect      @"orgasm.mp3"
+#define BonusEffect        @"starSound.mp3"
+#define YouLoseEffect      @"babyCry.mp3"
+#define BossDeadEffect     @"bossDead.mp3"
+
+//define Background Images
+#define MenuBackground                  @"menu320.png"
+#define MenuBackgroundIphone5           @"menu1136.png"
+#define HighscoresBackgroundIphone5     @"pausemenu_angie.png"
+#define HighscoresBackground            @"pausemenu_angie.png"
+#define GameOverBackground              @"pausemenu_angie.png"
+#define GameOverBackgroundIphone5       @"pausemenu_angie.png"
+#define PauseMenuBackground             @"pausemenu_small.png"
+#define PauseMenuBackgroundIphone5      @""
+#define Level1Background                @"forest_1.png"                     //"bg2.png"
+#define Level1BackgroundIphone5         @"forest_2.png"                     //"bg2.png"
+#define Level2Background                @"city_1.png"                       //"bg3.png"
+#define Level2BackgroundIphone5         @"city_2.png"                       //"bg3.png"
+#define Level3Background                @"desert_1.png"                     //"bg4.png"
+#define Level3BackgroundIphone5         @"desert_2.png"                     //"bg4.png"
+#define Level4Background                @"beach_1.png"                      //"bg5.png"
+#define Level4BackgroundIphone5         @"beach_2.png"                      //"bg5.png"
+#define Level5Background                @"africa_1.png"                     //"bg2.png"
+#define Level5BackgroundIphone5         @"africa_2.png"                     //"bg2.png"
+#define Level6Background                @"bedroom_1.png"                    //"bg3.png"
+#define Level6BackgroundIphone5         @"bedroom_2.png"                    //"bg3.png"
+#define Level7Background                @"cloud_city_1.png"                 //"bg4.png"
+#define Level7BackgroundIphone5         @"cloud_city_2.png"                 //"bg4.png"
+#define Level8Background                @"livingroom_1.png"                 //"bg5.png"
+#define Level8BackgroundIphone5         @"livingroom_2.png"                 //"bg5.png"
+#define LevelBonusBackground            @"bg3.png"
+#define LevelBonusBackgroundIphone5     @"bg3_ipad.png"
+
+//define player image
+#define PlayerImage      @"condom.png"
+
+//define bomb items
+#define BombSprayImage   @"spray.png"
+#define BombSpraySound   @"scifi003.mp3"
+
+//define enemy images
+#define EnemySpermDefault   @"sperm_default.png"      
+#define EnemySpermPlist     @"sperm_default.plist"  
+#define EnemySpermFrames    @"sp%d.png"
+
+#define FatEnemySpermDefault   @"fat_headed_sperm.png"
+#define FatEnemySpermPlist     @"fat_headed_sperm.plist"
+#define FatEnemySpermFrames    @"fs%d.png"
+
+#define EnemyVirusDefault   @"virus.png"
+#define EnemyVirusPlist     @"virus.plist"
+#define EnemyVirusFrames    @"virus_%d.png"
+
+#define EnemyBossDefault    @"big_boss.png"
+#define EnemyBossPlist     @"big_boss.plist"
+#define EnemyBossFrames    @"boss%d.png"
+
+//define Messages
+#define WinMessage          @"Thank God this is not your baby! \nMore levels cumming soon \nScore: %i"
+#define LoseMessage         @"Hello Daddy...!!! \nShake your iphone to make it sleep. \nScore: %i"
+
+//define Menu Images
+#define PauseButton         @"Settings.png"
+#define BombButton          @"spray60.png"
+#define PauseMenuBg         @"pausemenu_small.png"
+#define PauseResumeButton   @"resume_angie.png"
+
+#define MainMenuBackButton  @"back_angie.png"
+
+#define StartGameButton     @"startgame_angie.png"
+#define HighScoresButton    @"highscores_angie.png"
+
+
+
+
+
