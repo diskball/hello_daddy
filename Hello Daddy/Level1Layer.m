d8a2989aa4aeb1c1b044404ae432cf562ff0500d
George Bafaloukas <george.bafaloukas@iteam.gr>
2012-12-28 14:06:11 +0200

    fixing delays

--- Hello Daddy/Level1Layer.m
+++ Hello Daddy/Level1Layer.m
@@ -296,7 +296,7 @@
                 [[SimpleAudioEngine sharedEngine] stopBackgroundMusic];
                 [[SimpleAudioEngine sharedEngine] playEffect:LoadingEffect];
                 [self performSelector:@selector(startLevel)
-                           withObject:nil afterDelay:3.0f];
+                           withObject:nil afterDelay:1.0f];
             }
         }
         
